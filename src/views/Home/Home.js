61240194d23c97b9afa35621001ab3e7944ec8c8
sanketbhure <sanketbhure1306@gmail.com>
2024-03-28 14:14:56 +0530

    basic info added

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,8 +2,23 @@ import React from 'react'
 import "./Home.css"
 
 function Home() {
+
+    const usersCount = 15;
+
   return (
-    <div>Home</div>
+    <div>
+        <h1>Welcome!</h1>
+
+        <p>
+            We hope you enjoy learning React.
+        </p>
+
+        <h3>Have a great day!</h3>
+
+        <p>
+            {usersCount} users are learning in this session.
+        </p>
+    </div>
   )
 }
 
